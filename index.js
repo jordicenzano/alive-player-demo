edcd45de7e56cde1a3d0194e0724132e16427565
Jordi Cenzano <jordi.cenzano@gmail.com>
2017-03-19 23:34:35 +0100

    Add body parser json

--- index.js
+++ index.js
@@ -4,11 +4,16 @@
 
 const fs = require('fs');
 
+var bodyParser = require('body-parser');
+
 const PORT = 8080;
 const LOCAL_DATA_FILENAME = 'eventdata.json';
 
 var express = require('express');
 var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //Serve static from public
 app.use(express.static('public'));
